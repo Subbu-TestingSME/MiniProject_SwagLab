33d6c2404ff81af2b5b00338084d7bea97567115
Subbu-TestingSME <subbulakshmi.s@mphasis.com>
2025-12-17 15:29:38 +0530

    Third commit

--- MIniProject_SwagLab/src/test/java/utils/ExcelUtils.java
+++ MIniProject_SwagLab/src/test/java/utils/ExcelUtils.java
@@ -5,13 +5,12 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import java.io.FileInputStream;
 
 public class ExcelUtils {
-    public static String getCellData(String filePath, String sheetName, int row, int col) {
-        try (FileInputStream fis = new FileInputStream(filePath);
-             Workbook wb = new XSSFWorkbook(fis)) {
-            return wb.getSheet(sheetName).getRow(row).getCell(col).getStringCellValue();
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
+	public static String getCellData(String filePath, String sheetName, int row, int col) {
+		try (FileInputStream fis = new FileInputStream(filePath); Workbook wb = new XSSFWorkbook(fis)) {
+			return wb.getSheet(sheetName).getRow(row).getCell(col).getStringCellValue();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 }
